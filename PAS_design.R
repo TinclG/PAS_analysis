8641364c70fcffb02d163ca180ddf2e1ee148ae8
TinclG <tina.gorsek@medunigraz.at>
2022-07-04 14:03:39 +0200

    changing the organisation

--- PAS_design.R
+++
@@ -1,9 +0,0 @@
-PAS_design<-function(x, PAS = PAS, anno = anno){
-  dds<-DESeqDataSetFromMatrix(countData = PAS, 
-                              colData = anno, 
-                              design = x)
-  keep<-rowSums(counts(dds)) >=10
-  dds<-dds[keep,]
-  
-  return(dds)
-}
