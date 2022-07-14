80090fe4f62764dfb1cba77e141eddb6ee6ee25e
TinclG <tina.gorsek@medunigraz.at>
2022-07-14 14:18:32 +0200

    "Changing folder names"

---
+++ lib/Data_wrangling.R
@@ -0,0 +1,24 @@
+#-------------Useful commands for data wrangling dplyr---------
+#-------------Params-------------------------------------------
+params:
+  data: "../data"
+results: "../res"
+lib: "../lib"
+
+params$data 
+### example paste0(params$data,"/metadata_PASeq.csv")
+
+#-------------Importing the file-------------------------------
+#csv 
+anno <- read.csv2(paste0(params$data,"/metadata_PASeq.csv"),
+                  sep= ";")
+#tables
+PAS<-read_tsv(paste0(params$data, "/PAS_counts.tsv"))
+
+#-------------Useful dplyr functions with examples------------
+PAS <- PAS |> 
+  mutate(gene_id = gsub("\\.[1-9]*$", "", gene_id)) |> 
+  select(-gene_name) |>
+  mutate_at(vars(-("gene_id")), ceiling) |> #rounding the counts
+  rename_with(~str_replace(., pattern = "AK19_", replacement = "")) |>
+  column_to_rownames(var = "gene_id")
