87e1517585b8a53ff736a97b36273d321540af1d
TinclG <tina.gorsek@medunigraz.at>
2022-07-19 15:46:30 +0200

    Adding new functions

--- lib/Data_wrangling.R
+++ lib/Data_wrangling.R
@@ -15,10 +15,16 @@ anno <- read.csv2(paste0(params$data,"/metadata_PASeq.csv"),
 #tables
 PAS<-read_tsv(paste0(params$data, "/PAS_counts.tsv"))
 
-#-------------Useful dplyr functions with examples------------
+#-------------Useful dplyr functions with examples-------------
 PAS <- PAS |> 
   mutate(gene_id = gsub("\\.[1-9]*$", "", gene_id)) |> 
   select(-gene_name) |>
   mutate_at(vars(-("gene_id")), ceiling) |> #rounding the counts
   rename_with(~str_replace(., pattern = "AK19_", replacement = "")) |>
-  column_to_rownames(var = "gene_id")
+  column_to_rownames(var = "gene_id") |>
+  select(-columnname) #removing a column
+
+
+#------------Removing duplicated features----------------------
+
+mat <- mat[!duplicated(mat$gene_name),] 
