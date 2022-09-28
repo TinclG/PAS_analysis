f731bcc2cd71f0ec20009490e49b9a1731aa155b
TinclG <tina.gorsek@medunigraz.at>
2022-09-28 12:45:03 +0200

    Color palette

--- lib/Visualisation_codes.R
+++ lib/Visualisation_codes.R
@@ -3,3 +3,11 @@
 #------nice color palettes-------
 
 #https://github.com/kevinsblake/NatParksPalettes
+
+
+#-----colours used for the RNAseq analysis---sept2022
+
+##PCA 
+library(viridis)
+#https://waldyrious.net/viridis-palette-generator/
+magma <-c("#fcfdbf", "#fe9f6d", "#de4968", "#8c2981",  "#3b0f70")
